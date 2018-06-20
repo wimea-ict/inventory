b1d9bef146d0ddbfd1c1dc8001877d4feff2cd5b
Robert Odoch <robertelvisodoch@gmail.com>
2018-06-20 15:31:07 +0300

    Add project assets.

---
+++ js/inventory.js
@@ -0,0 +1,10 @@
+$('#more-items').click(function(event) {
+    event.preventDefault();
+    
+    $('#item').clone().appendTo($('#items')).css({marginTop: '5px'});
+    $('#quantity').clone().val('').appendTo($('#quantities')).css({marginTop: '5px'});
+});
+
+$('#data-table').DataTable({
+    responsive: true
+});

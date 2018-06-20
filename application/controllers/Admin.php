6d49da8d0778fea9367f03812ae63c9faa219544
Robert Odoch <robertelvisodoch@gmail.com>
2018-06-20 22:54:47 +0300

    Only allow logged in users to view the dashboard.

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -4,6 +4,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Admin extends CI_Controller {
     public function __construct() {
         parent::__construct();
+
+        if ($this->session->has_userdata('user') == false) {
+            redirect(base_url('auth/login'));
+        }
     }
 
     public function dashboard() {

36c9b7b5ec82b5f3f903b4e16d90c1b7c3cd6132
Robert Odoch <robertelvisodoch@gmail.com>
2018-06-20 20:12:58 +0300

    Unset session message.
    CodeIgniter flashdata not unsetting some messages.

--- application/views/categories/all.php
+++ application/views/categories/all.php
@@ -8,7 +8,10 @@ require_once('page-header.php');
     <div class="col-lg-12">
         <?php if(isset($_SESSION['message'])): ?>
             <div class="alert alert-<?= $_SESSION['message_class']; ?>"><?= $_SESSION['message']; ?></div>
-        <?php endif; ?>
+        <?php
+            unset($_SESSION['message']);
+            endif;
+        ?>
 
         <!-- Categories -->
         <table width="100%" class="table table-striped table-bordered table-hover" id="data-table">
